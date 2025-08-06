abd29e54a2dc311442c910744463b4ac8de41a7b
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-06 19:42:53 +0530

    add The MongoDB Connection

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
+
 
 app.use(express.json());
 app.use(cookieParser());
