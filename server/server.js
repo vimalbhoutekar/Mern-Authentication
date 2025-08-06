ad71260fcd808342e26634eb57a53ac0c997ac5b
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-06 19:00:35 +0530

    Setup The Project With make client and server Folder and Make the server.js File and Run the Express App

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
+
