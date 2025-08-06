75ba6c98d40c637c04e7ac5cb04577c1eaa93ffb
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-06 19:11:38 +0530

    Modify the package.json file And Basic get method use server.js file

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+app.get('/', (req, res) => {
+  res.send('Hello Welcome to the MERN Auth Server');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
