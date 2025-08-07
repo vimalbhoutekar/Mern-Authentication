fa3e4c47da170b51e2fa5721c464c31c31765716
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-08 01:15:01 +0530

    Create Verify Email Controller Function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -128,3 +128,36 @@ export const sendVerifyOtp = async (req, res) => {
         return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
+
+export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body;
+    if(!userId || !otp) {
+        return res.status(400).json({ success: false, message: "Missing Details" });
+    }   
+
+    try{
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        if(user.verifyOtp === "" || user.verifyOtp !== otp)
+        {
+            return res.status(400).json({ success: false, message: "Invalid OTP" });
+        }
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.status(400).json({ success: false, message: "OTP expired" });
+        }   
+        user.isAccountVerified = true;
+        user.verifyOtp = "";
+        user.verifyOtpExpireAt = 0;
+        await user.save();
+
+        return res.status(200).json({ success: true, message: "Email verified successfully" });
+    }
+    catch (error) {
+        console.error("Error in verifying email:", error);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }   
+
+}
+
