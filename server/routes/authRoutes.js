5094931430461362d40d4d884aab040658f105d1
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-09 11:55:21 +0530

    add the is-auth route in authRouter

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,login,logout, sendVerifyOtp, verifyEmail } from "../controllers/authController.js";
+import { register,login,logout, sendVerifyOtp, verifyEmail, isAuthenticated } from "../controllers/authController.js";
 import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
@@ -9,5 +9,7 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp",userAuth,sendVerifyOtp);
 authRouter.post("/verify-account",userAuth,verifyEmail);
+authRouter.post("/is-auth",userAuth,isAuthenticated);
+
 
 export default authRouter;
