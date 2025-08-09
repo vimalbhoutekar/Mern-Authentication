9221877923c1efce62a6e85e839f78fee2c5a263
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-09 12:45:34 +0530

    Create getUserData Controller Function in userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+import User from "../models/userModel";
+
+export const getUserData = async (req,res) => {
+    try{
+        const {userId} = req.body;
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({success: false, message: "User not found"});
+        }
+        res.status(200).json({success: true, userData : {
+            name : user.name,
+            isAccountVerified : user.isAccountVerified
+        }});  
+    }
+    catch(error) {
+        res.status(500).json({success: false, message: error.message });
+    }
+}
