9edfaadd7d17bcc51be58c3a37fcfa94832092be
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-09 12:23:52 +0530

    Create a ResetPassword Controller Function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -212,3 +212,35 @@ export const sendResetOtp = async (req,res) =>
     }
 }
 
+// Reset User Password 
+export const resetPassword = async (req,res) =>
+{
+    const {email,otp,newPassword} = req.body;
+    if(!email || !otp || !newPassword)
+    {
+        return res.status(400).json({success:false,message:"All fields are required"});
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        if (user.resetOtp === "" || user.resetOtp !== otp) {
+            return res.status(400).json({ success: false, message: "Invalid OTP" });
+        }
+        if (user.resetOtpExpireAt < Date.now()) {
+            return res.status(400).json({ success: false, message: "OTP expired" });
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        user.resetOtp = "";
+        user.resetOtpExpireAt = 0;
+        await user.save();
+
+        return res.status(200).json({ success: true, message: "Password reset successfully" });
+    } catch (error) {
+        console.error("Error in resetting password:", error);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }  
+}
