dbc958918264ef66ae601bde0e0b558a578990c1
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-11 23:54:28 +0530

    add backend url in AppContext and add some other states

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,6 +3,15 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [isLoggedin,setIsLoggedin] = useState(false);
+    const [userData,setUserData] = useState(false);
+  const value = {
+    backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
