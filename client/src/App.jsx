a25833fa5d749a1793bda0c5912992247901b760
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-11 15:11:33 +0530

    Create Routes for Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
   )
 }
