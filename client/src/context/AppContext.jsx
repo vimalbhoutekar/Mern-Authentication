95e880cb607e36c0ae371dd352ad0317b8de5d2e
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2025-08-11 23:43:21 +0530

    Create a AppContext using createContext()

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const value = {};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
